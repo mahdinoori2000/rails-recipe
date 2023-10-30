181cfc202c04f0a90fcc1b67879c6fb9b41c09ed
Alfred M <alfred.mkg@proton.me>
2023-10-31 01:13:30 +0300

    Update food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,9 @@
 class Food < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :preparation_time, presence: true
+  validates :cooking_time, presence: true
+  validates :description, presence: true
+  validates :public, presence: true
 end

2aa5f9399161b40be512a7659ebac6669da3010e
Mahdi Noori <mahdinoori2389@gmail.com>
2023-11-01 11:13:52 +0430

    Update recipe model.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,11 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :recipe_foods
+  has_many :foods, through: :recipe_foods
 
   validates :name, presence: true
-  validates :quantity, presence: true
-  validates :measurement_unit, presence: true
-  validates :price, presence: true
+  validates :preparation_time, presence: true
+  validates :cook_time, presence: true
+  validates :description, presence: true
   validates :public, presence: true
 end

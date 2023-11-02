9e2325fcf3c208dc99de90fe92099da8767333a0
Mahdi Noori <mahdinoori2389@gmail.com>
2023-11-02 18:55:25 +0430

    Add delete route

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   get 'recipe_foods' => 'recipe_foods#index', as:'recipe_foods'
   get 'recipe_foods/new' => 'recipe_foods#new', as:'add_recipe_food'
   post 'recipe_foods/create' => 'recipe_foods#create', as:'create_recipe_food'
+  get 'recipe_foods/delete/:id' => 'recipe_foods#destroy', as: 'delete_recipe_food'
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
